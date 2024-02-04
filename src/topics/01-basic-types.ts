2fb83954c969c058fc9bbb132a5a3b91810d71d9
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2024-02-04 15:06:29 -0500

    first commit

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,13 @@
+const name: string = 'The crilogo'; 
+
+let hpPoints: number | string = 95;
+
+const isAlive: boolean = true;
+
+hpPoints = 'FULL'
+
+
+console.log({name, isAlive, hpPoints})
+
+
+export {};
